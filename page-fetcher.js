47e019172de413c9bbd6e69bef8405e6fb918512
Christopher <cristopherng@hotmail.com>
2019-08-28 19:41:00 -0700

    edge cases accounted for

--- page-fetcher.js
+++ page-fetcher.js
@@ -1,21 +1,22 @@
+
 const request = require('request');
 const getArgv = process.argv.splice(2);
 const fs = require('fs');
 
 const fetcher = function(url, path) {
   const write = function(callback) {
-    fs.writeFile(path, callback, () => {
-      console.log("the webpage has been downloaded");
+    fs.writeFile(path, callback, (error) => {
+      if (error) {
+        console.log(`${error} \nWriteFile could not be completed.`);
+        process.exit;
+      } else console.log(`the webpage has been downloaded at ${path}`);
     });
   };
   request(url, (error, response, body) => {
-    if (error === null || error >= 200 && error <= 300) {
+    if (error === null || response.statusCode >= 200 && response.statusCode <= 300) {
       write(body);
     } else console.log(`Error ${error} detected. File will not be written to`);
   });
 };
 
 fetcher(getArgv[0], getArgv[1]);
-
-
-
