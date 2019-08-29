d7a4aeb45cfa2a43ac7e70c14e8dd597df719b8d
Christopher <cristopherng@hotmail.com>
2019-08-28 18:46:04 -0700

    page-fetcher.js refactored

--- page-fetcher.js
+++ page-fetcher.js
@@ -2,29 +2,6 @@ const request = require('request');
 const getArgv = process.argv.splice(2);
 const fs = require('fs');
 
-
-// const write = function(callback, path) {
-//   fs.writeFile(path, callback, () => {
-//     console.log("the webpage has been downloaded");
-//   });
-
-// };
-
-
-// const fetcher = function(url, path) {
-//   // request(getArgv, (error, response, body) => {
-//   //   console.log('error:', error); // Print the error if one occurred
-//   //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   //   console.log('body:', body); // Print the HTML for the Google homepage.
-//   //   return body;
-//   // });
-//   write(request(getArgv, (body) => {
-//     return body;
-//   }), path);
-// };
-
-
-
 const fetcher = function(url, path) {
   const write = function(callback) {
     fs.writeFile(path, callback, () => {
